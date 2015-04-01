6e0907fb94c686a06911f02e8926bdab8bfb5437
chitterchitters <chitterchitters@gmail.com>
2015-03-31 22:37:25 -0400

    Switched to pre-renewal ATK speed values

--- src/config/renewal.h
+++ src/config/renewal.h
@@ -81,7 +81,7 @@
 /// - AGI has a greater factor in ASPD increase
 /// - there is a change in how skills/items give ASPD
 /// - some skill/item ASPD bonuses won't stack
-#define RENEWAL_ASPD
+//#define RENEWAL_ASPD
 
 #endif // DISABLE_RENEWAL
 #undef DISABLE_RENEWAL
