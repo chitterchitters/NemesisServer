7801ec9071272745e663af59bb5687975d0c925e
darkwolf7786 <darkwolf7786@yahoo.com>
2015-04-02 20:27:21 -0400

    Revert "Adjusting base MATK calculations Part 2."
    This reverts commit 69409d14b6f361fe6007fce7a01f1b2d5dc37590.

--- src/map/status.c
+++ src/map/status.c
@@ -4124,7 +4124,7 @@ unsigned short status_base_matk(struct block_list *bl, const struct status_data 
 			return st->int_ + st->int_ / 5 * st->int_ / 5;
 		case BL_PC:
 		default: // temporary until all are formulated
-			return st->int_ + ((st->int_ / 5)*(st->int_ / 5)) + (st->dex / 3) + (st->luk) + (level / 2);
+			return st->int_ + (st->int_ * 5) + (st->dex / 3) + (st->luk) + (level / 2);
 	}
 #else
 	return 0;
